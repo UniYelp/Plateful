d85a5c4e689709ba0cbe4e39d7a12ee999d10930
levyyeh <yehonatanlevy13@gmail.com>
2025-12-02 11:34:40 +0200

    chore: add conversions

--- apps/web/src/features/ingredients/constants.ts
+++ apps/web/src/features/ingredients/constants.ts
@@ -22,11 +22,48 @@ type IngredientsUnitConversion = UnitConversion<
  * ? Prefer conversions from a "lower" unit to an "upper" unit (>=1)
  */
 export const ingredientsUnitConversions: IngredientsUnitConversion[] = [
+	// #region Mass
 	{
 		from: "g",
 		to: "kg",
 		by: 1000,
 	},
-	{ from: "mL", to: "L", by: 1000 },
 	{ from: "oz", to: "g", by: 28.3495 },
+	{ from: "lb", to: "oz", by: 16 },
+	// #endregion
+	// #region Volume
+	{ from: "mL", to: "L", by: 1000 },
+	{
+		from: "tsp",
+		to: "dsp",
+		by: 2,
+	},
+	{
+		from: "tsp",
+		to: "tbsp",
+		by: 3,
+	},
+	{
+		from: "tsp",
+		to: "mL",
+		by: 5 /** @note metric/imperial tsp | US tsp is by 4.93 */,
+	},
+	{
+		from: "tbsp",
+		to: "mL",
+		by: 15 /** @note metric/imperial tsp | US tsp is by 14.79 */,
+	},
+	{
+		from: "cup",
+		to: "tbsp",
+		by: 16 /** @note metric/imperial tsp | US tsp is by 14.79 */,
+	},
+	{
+		from: "cup",
+		to: "mL",
+		by: 240,
+	},
+	// #endregion
+	// #region Temperature
+	// #endregion
 ];
--- apps/web/src/features/units/enums/unit.enum.ts
+++ apps/web/src/features/units/enums/unit.enum.ts
@@ -1,14 +1,26 @@
 import type { ValueOf } from "@plateful/types";
 
 export const Unit = {
+	// #region Mass
 	Gram: "Gram",
 	Kilogram: "Kilogram",
 	Ounce: "Ounce",
+	Pound: "Pound",
+	// #endregion
+	// #region Volume
 	Milliliter: "Milliliter",
 	Liter: "Liter",
+	Teaspoon: "Teaspoon",
+	Dessertspoon: "Dessertspoon",
+	Tablespoon: "Tablespoon",
+	Cup: "Cup",
+	// FluidOunce: "FluidOunce",
+	// #endregion
+	// #region Temperature
 	Celsius: "Celsius",
 	Fahrenheit: "Fahrenheit",
 	Kelvin: "Kelvin",
+	// #endregion
 } as const;
 
 export type Unit = ValueOf<typeof Unit>;
