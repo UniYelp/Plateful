90313987ffdecdb45a6e009417e8d88126b613a7
levyyeh <yehonatanlevy13@gmail.com>
2025-12-15 21:13:31 +0200

    fixes

--- apps/web/src/configs/api.config.ts
+++ apps/web/src/configs/api.config.ts
@@ -1,6 +1,6 @@
 import { treaty } from "@elysiajs/eden";
 
-import type { App as ApiServer } from "@plateful/api";
+import type { App as ApiServer } from "@api/client";
 import { ENV } from "./env.config";
 
 export const apiClient = treaty<ApiServer>(ENV.VITE_API_URL);
