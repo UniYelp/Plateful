639abe85b3d5a36d03c88f08b921e7bb9451bff7
elirazpevz <elirazp@post.bgu.ac.il>
2025-12-20 20:35:48 +0200

    we love best practices

--- apps/backend/convex/external_user.ts
+++ apps/backend/convex/external_user.ts
@@ -6,61 +6,67 @@ import { action } from "./_generated/server";
 import { type MemberRole, vv } from "./schema";
 
 const clerk = createClerkClient({
-	secretKey: process.env.CLERK_SECRET_KEY,
+  secretKey: process.env.CLERK_SECRET_KEY,
 });
 
 export const getHouseholdMembersData = action({
-	args: {
-		householdId: vv.id("households"),
-	},
-	handler: async (ctx, args) => {
-		const members = await ctx.runQuery(api.households.getHouseholdMembers, {
-			householdId: args.householdId,
-		});
+  args: {
+    householdId: vv.id("households"),
+  },
+  handler: async (ctx, args) => {
+    const members = await ctx.runQuery(api.households.getHouseholdMembers, {
+      householdId: args.householdId,
+    });
 
-		const memberDetails = await Promise.all(
-			members.map(async (member) => ({
-				...member,
-				externalId: await ctx.runQuery(
-					internal.users.getExternalUserIdByUserId,
-					{
-						userId: member.userId,
-					},
-				),
-			})),
-		);
+    const memberIds = members.map((member) => member.userId);
 
-		const memberDetailsByExternalId: Record<
-			string,
-			{ id: Id<"users">; role: MemberRole }
-		> = Object.fromEntries(
-			memberDetails.map(({ externalId, userId, role }) => [
-				externalId,
-				{
-					id: userId,
-					role,
-				},
-			]),
-		);
+    const memberExternalIds = await ctx.runQuery(internal.users.externalUserIdListByUserIds, {
+      userIds: memberIds,
+    });
 
-		const userDetail = await clerk.users.getUserList({
-			userId: Object.keys(memberDetailsByExternalId),
-		});
+    const externalIdByUserId = Object.fromEntries(
+      memberExternalIds.map(({ externalId, userId }) => [
+        userId,
+        {
+          externalId: externalId,
+        },
+      ]),
+    );
 
-		const mergedData = userDetail.data.map((user) => {
-			const memberDetails = memberDetailsByExternalId[user.id];
+    const memberDetails = members.map((member) => ({
+      ...member,
+      externalId: externalIdByUserId[member.userId].externalId,
+    }));
 
-			return {
-				...memberDetails,
-				fullName: user.fullName,
-				firstName: user.firstName,
-				lastName: user.lastName,
-				email: user.primaryEmailAddress?.emailAddress,
-			};
-		});
+    const memberDetailsByExternalId: Record<string, { id: Id<"users">; role: MemberRole }> =
+      Object.fromEntries(
+        memberDetails.map(({ externalId, userId, role }) => [
+          externalId,
+          {
+            id: userId,
+            role,
+          },
+        ]),
+      );
 
-		return mergedData;
-	},
+    const userDetail = await clerk.users.getUserList({
+      userId: Object.keys(memberDetailsByExternalId),
+    });
+
+    const mergedData = userDetail.data.map((user) => {
+      const memberDetails = memberDetailsByExternalId[user.id];
+
+      return {
+        ...memberDetails,
+        fullName: user.fullName,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.primaryEmailAddress?.emailAddress,
+      };
+    });
+
+    return mergedData;
+  },
 });
 
 // helpers
