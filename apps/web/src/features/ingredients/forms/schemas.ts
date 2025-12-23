fb06ae4ec241787490f0a5c4190ace22bf2586d9
elirazpevz <elirazp@post.bgu.ac.il>
2025-12-23 17:19:13 +0200

    move magic numbers to constants

--- apps/web/src/features/ingredients/forms/schemas.ts
+++ apps/web/src/features/ingredients/forms/schemas.ts
@@ -2,9 +2,13 @@ import z from "zod";
 
 import { IngredientUnit } from "@plateful/ingredients";
 import {ingredientCategoriesMap } from "@/pages/dashboard/ingredients";
+
+const INGREDIENT_MAXIMUM_NAME_LENGTH = 30;
+const INGREDIENT_MINIMUM_NAME_LENGTH = 2;
+const INGREDIENT_MAXIMUM_DESCRIPTION_LENGTH = 200;
 export const AddIngredientFormSchema = z.object({
-	name: z.string().min(2).max(30),
-	description: z.string().max(200).optional(),
+	name: z.string().min(INGREDIENT_MINIMUM_NAME_LENGTH).max(INGREDIENT_MAXIMUM_NAME_LENGTH),
+	description: z.string().max(INGREDIENT_MAXIMUM_DESCRIPTION_LENGTH).optional(),
 	amount: z.number().min(0),
 	unit: z.enum(IngredientUnit).optional(),
 	category: z.enum(ingredientCategoriesMap),
