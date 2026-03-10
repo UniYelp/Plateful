421c6cb141c9560d3e386662339ca1e6327b2208
levyyeh <yehonatanlevy13@gmail.com>
2026-03-10 19:05:56 +0200

    fix env setup

--- apps/backend/scripts/env.mjs
+++ apps/backend/scripts/env.mjs
@@ -1,14 +1,46 @@
 #!/usr/bin/env node
 
-import { writeFileSync } from "node:fs";
-import { join } from "node:path";
+import { execSync } from "node:child_process";
 import { createEnv } from "@t3-oss/env-core";
+import { vercel } from "@t3-oss/env-core/presets-zod";
+import { withRelatedProject } from "@vercel/related-projects";
 import z from "zod";
 
-import {withRelatedProject} from "@vercel/related-projects";
+const vercelEnv = createEnv({
+	extends: [vercel()],
+	server: {},
 
+	/**
+	 * The prefix that client-side variables must have. This is enforced both at
+	 * a type-level and at runtime.
+	 */
+	clientPrefix: undefined,
 
-export const ENV = createEnv({
+	client: {},
+
+	/**
+	 * What object holds the environment variables at runtime. This is usually
+	 * `process.env` or `import.meta.env`.
+	 */
+	runtimeEnv: process.env,
+
+	/**
+	 * By default, this library will feed the environment variables directly to
+	 * the Zod validator.
+	 *
+	 * This means that if you have an empty string for a value that is supposed
+	 * to be a number (e.g. `PORT=` in a ".env" file), Zod will incorrectly flag
+	 * it as a type mismatch violation. Additionally, if you have an empty string
+	 * for a value that is supposed to be a string with a default value (e.g.
+	 * `DOMAIN=` in an ".env" file), the default value will never be applied.
+	 *
+	 * In order to solve these issues, we recommend that all new projects
+	 * explicitly specify this option as true.
+	 */
+	emptyStringAsUndefined: true,
+});
+
+const ENV = createEnv({
 	extends: [],
 	server: {
 		//? Api
@@ -56,25 +88,21 @@ export const ENV = createEnv({
 	emptyStringAsUndefined: true,
 });
 
-
 const apiHost = withRelatedProject({
-  projectName: 'plateful-api',
-  /**
-   * Specify a default host that will be used for my-api-project if the related project
-   * data cannot be parsed or is missing.
-   */
-  defaultHost: ENV.API_URL,
+	projectName: "plateful-api",
+	/**
+	 * Specify a default host that will be used for my-api-project if the related project
+	 * data cannot be parsed or is missing.
+	 */
+	defaultHost: ENV.API_URL,
 });
-
-const envPath = join(import.meta.dirname, "../.env.defaults");
 
 ENV.API_URL = apiHost;
 
-writeFileSync(
-	envPath,
-	Object.entries(ENV)
-		.filter(([, value]) => value !== undefined)
-		.map(([key, value]) => `${key}=${value}`)
-		.join("\n"),
-	"utf-8",
-);
+const isProd = vercelEnv.VERCEL_ENV === "production";
+
+for (const [key, value] of Object.entries(ENV)) {
+	if (value === undefined) continue;
+
+	execSync(`convex env set ${isProd ? "--prod" : ""} ${key} ${value}`);
+}
